22ac1c3d4f2a405dd7ebfa99695e68112cce8cb9
Williampetersen <williampetersencop@gmail.com>
2026-05-19 17:10:27 +0200

    Updated website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://washmax.dk",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
